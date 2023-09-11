aa8c7daeb5567151be23e63605470ec8ea8d7cb0
aholstrup1 <aholstrup1@users.noreply.github.com>
2023-09-11 13:31:33 +0000

    Deploying AL-Go from main (e420efdb70c5b02e9f3cae915309cd3084041682) to main

--- CodeAnalysis/Codeanalysis.ps1
+++ CodeAnalysis/Codeanalysis.ps1
@@ -5,4 +5,9 @@ param(
 
 Import-Module $PSScriptRoot/ErrorlogToSarif.psm1
 
-ConvertTo-SarifLog -Path $Path -OutputPath $OutputPath
+$logfiles = Get-ChildItem -Path $Path -Filter errorLog.json -Recurse
+Write-Host "Found $($logfiles.Count) errorlog.Json files"
+$logfiles | ForEach-Object {
+    Write-Host "Processing $($_.FullName)"
+    ConvertTo-SarifLog -Path $_.FullName -OutputPath $OutputPath
+} 
